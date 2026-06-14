3cd71d4fd1c3d6fda5dac3e8489ce03f6b5e72cc
MarketX <info@mxaccelerator.com>
2026-06-15 01:15:52 +0200

    Configure Astro for GitHub Pages subdirectory deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://marioolivelli.github.io/marketx-site/',
   integrations: [react()],
   trailingSlash: 'never',
+  output: 'static',
 });
